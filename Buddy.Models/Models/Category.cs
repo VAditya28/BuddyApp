ee7eedec9c88203e9144b3e92caee5ee14c7103b
= <varun300515@gmail.com>
2025-11-17 15:04:48 -0600

    Buddy Book Models

---
+++ Buddy.Models/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace Buddy.Models.Models
+{
+    public class Category 
+    {
+        [Key]
+        public int category_Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage ="Exceeded length")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(0, 100,ErrorMessage ="Must be 1 to 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
